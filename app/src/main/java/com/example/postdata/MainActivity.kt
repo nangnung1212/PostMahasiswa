fbf93a0f51f97f8a18d267fd4f4c75167be1267a
Gunung <arizonagabriel12@gmail.com>
2022-10-20 09:20:25 +0700

    UTS Pemrograman Mobile

--- app/src/main/java/com/example/postdata/MainActivity.kt
+++ app/src/main/java/com/example/postdata/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.postdata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import retrofit2.Call
@@ -15,6 +17,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val BtnDelete = findViewById<Button>(R.id.btn_delete)
+        BtnDelete.setOnClickListener {
+            val Intent = Intent( this,ContextMenuDel::class.java)
+            startActivity(Intent)
+        }
+
+        val GetDataBtn = findViewById<Button>(R.id.btn_get_data)
+        GetDataBtn.setOnClickListener {
+            val Intent = Intent(this,GetData::class.java)
+            startActivity(Intent)
+        }
     }
 }
 
